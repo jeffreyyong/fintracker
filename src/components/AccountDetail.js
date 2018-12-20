9fd82d05e8c3f9d252c8a441897d1863549aa12e
jeffreyyong <jeffreyyong10@gmail.com>
2018-12-20 23:38:01 +0000

    Need to add logo

--- src/components/AccountDetail.js
+++ src/components/AccountDetail.js
@@ -1,13 +1,78 @@
-// import React from 'react';
-// import Cardf
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './common/Card';
+import CardSection from './common/CardSection';
+import Button from './common/Button';
 
-// const AccountDetail = ({ account }) => {
-//     const { name, balance, last_updated, logo } = account;
+const AccountDetail = ({ account }) => {
+    const { name, balance, last_updated, logo } = account;
+    const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        balanceContainerStyle,
+        lastUpdatedTextStyle,
+        balanceTextStyle,
+        imageStyle
+    } = styles;
 
-//     return (
-//         <Card>
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: logo }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{name}</Text>
+                    <Text style={lastUpdatedTextStyle}>Updated {last_updated}</Text>
+                </View>
+                <View style={balanceContainerStyle}>
+                    <Text style={balanceTextStyle}>{balance}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
 
-//         </Card>
-//     )
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 15
+    },
+    lastUpdatedTextStyle: {
+        fontSize: 11
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    balanceContainerStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    balanceTextStyle: {
+        fontWeight: 'bold'
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
 
-// }
+export default AccountDetail;
--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -6,6 +6,24 @@ import AccountDetail from './AccountDetail';
 export default class Accounts extends Component {
     state = { accounts: [] };
 
+    componentWillMount() {
+        const data = [
+            {
+                name: 'Monzo UK Current Account',
+                balance: '£123.00',
+                last_updated: '1 hour ago',
+                logo: '../../asset/logo/monzo.jpg'
+            },
+            {
+                name: 'Lloyds Classic Current Account',
+                balance: '£2.49',
+                last_updated: 'just now',
+                logo: '../../asset/logo/lloyds.jpg)'
+            },
+        ];
+        this.setState({ accounts: data });
+    }
+
     renderAccounts() {
         return this.state.accounts.map(account =>
             <AccountDetail key={account.name} account={account} />
